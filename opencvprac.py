6909c08baa662ee5429662d6a20292553e9f88ad
cdy3870 <thefallen3870@yahoo.com>
2018-11-10 18:52:33 -0500

    Update opencvprac.py

--- opencvprac.py
+++ opencvprac.py
@@ -21,26 +21,51 @@ cap = cv2.VideoCapture(1)
 
 cap.release()
 cv2.destroyAllWindows()'''
+Myserial.write('N'.encode())
 while True:
   ret, img = cap.read()
   gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
   faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+  
   for (x,y,w,h) in faces:
     cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0), 2)
+    center = (x+w)/2
+    margin = 15;
+    
+    if center-margin < 220 and center+margin > 180 and len(faces) == 1:
+      print("centered")
+      Myserial.write('Y'.encode())
+      print('Y')
+      Myserial.write('N'.encode())
+      print('N')
+      
+      
+    elif(len(faces) == 1):
+      print("not centered")
+      if center+margin > 220:
+        Myserial.write('R'.encode())
+        print('R')
+      else:
+        Myserial.write('L'.encode())
+        print('L')
+
+    else:
+      Myserial.write('N'.encode())
     roi_gray = gray[y:y+h, x:x+w]
     roi_color = img[y:y+h, x:x+w]
     eyes = eye_cascade.detectMultiScale(roi_gray)
+    
     for(ex, ey, ew, eh) in eyes:
       cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0,255,0), 2)
 
   cv2.imshow('img', img)
   
-  if (len(faces)) == 0:
+  '''if (len(faces)) == 0:
     Myserial.write('N'.encode())
-    print ("No face is detected")
+    print('N')
   elif (len(faces)) == 1:
     Myserial.write('Y'.encode())
-    print ("Face is detected")
+    print('Y')'''
     
   key = cv2.waitKey(5) & 0xFF
     # tests if the q key has been pressed
@@ -51,4 +76,3 @@ while True:
 cap.release()
 
 cv2.destroyAllWindows()
-
