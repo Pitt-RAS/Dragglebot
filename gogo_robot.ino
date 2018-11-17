640dc280f5fc6f1e415e3a5ea613a8763c53eeb5
cdy3870 <thefallen3870@yahoo.com>
2018-11-17 01:38:22 -0500

    Update gogo_robot.ino

--- gogo_robot.ino
+++ gogo_robot.ino
@@ -50,34 +50,68 @@ void loop() {
   
   // look for a good SBUS packet from the receiver
   
-  if(x8r.read(&channels[0], &failSafe, &lostFrame)){
+  /*if(x8r.read(&channels[0], &failSafe, &lostFrame)){
     last_updated = millis();
     
-    if ( channels[5] == 1811 && count == 0 ) {
+    if ( channels[5] == 1811 && count!=1 ) {
       double throttle = channels[2] - 992;
       throttle /= 819.0;
   
-      double steering = 1811 - 992;
+      double steering = 1100 - 992;
       steering /= 819.0;
   
       double left_m = throttle + steering;
       double right_m = throttle - steering;
       Serial.println(channels[0]);
-      set_speeds(0.25, 0.25);
+      /*set_speeds(0.25, 0.25);
       delay(5000);
-      set_speeds(left_m, right_m);
-      delay(550);
+      for(int i = 0; i<10; i++){
+        set_speeds(left_m, right_m);
+        delay(100);
+      }
+      for(int i1 = 0; i1<10; i1++){
+        set_speeds(right_m, left_m);
+        delay(100);
+      }
       set_speeds(0, 0);
       delay(1000);
       count++;
     }
     else
       set_speeds(0, 0);
+       
   }
 
   if ( failSafe )
     set_speeds(0, 0);
 
-  if ( millis() - last_updated > 500 )
-    set_speeds(0, 0);
+  if ( millis() - last_updated > 500 ){
+    set_speeds(0, 0); 
+  }*/
+
+      double throttle = channels[2] - 992;
+      throttle /= 819.0; 
+      double steering = 1100 - 992;
+      steering /= 819.0;
+      double left_m = 0 + steering;
+      double right_m = 0 - steering; 
+         
+      if(Serial.read() == 'Y'){
+        set_speeds(0.2, 0.2);
+      }
+      else if(Serial.read() == 'N' || Serial.read() == 'R' || Serial.read() == 'L'){
+        set_speeds(0, 0);
+      }   
+      
+      if(Serial.read() == 'R'){
+        
+        set_speeds(0.3, -0.1);       
+        /*delay(25);*/
+      }
+      else if(Serial.read() == 'L'){
+        set_speeds(-0.1, 0.3);
+        
+      }
+      
+    
 }
